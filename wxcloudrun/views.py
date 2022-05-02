ccde5558187e2791b254edd9a86403f738bd4164
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-05-02 12:45:32 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -11,6 +11,7 @@ import pandas as pd
 import seaborn as sns
 import lightgbm as lgb
 
+#获取文件
 def getfile(fileid):
     response = requests.get( 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxfb2997f507abf89e&secret=168726b557fb7221c96955cec59b3347', )
     access_token = response.json()['access_token']
@@ -29,27 +30,18 @@ def getfile(fileid):
     response = requests.post("https://api.weixin.qq.com/tcb/batchdownloadfile?access_token=" + access_token,data,)
     print(response.json())
     download_url = response.json()['file_list'][0]['download_url']
+    filename =download_url.split("/")[-1]
     f = requests.get(download_url)
+    path = "/app/wxcloudrun/"+filename
     #下载文件
-    with open(r"/app/wxcloudrun/11.xlsx","wb") as code:
+    with open(path,"wb") as code:
          code.write(f.content)
+    return path
 
 @app.route('/', methods=['POST'])
 def upload():
     gbm = lgb.Booster(model_file='/app/wxcloudrun/model.txt')
     fileid = request.json.get('fileid')
-    
-    courseid = request.json.get('courseid')
-    text = jieba.lcut(file)
-    for item in text:
-        if item not in stopwords:
-            texts.append(item)
-    text = str(texts)
-    bg_pic = imread('/app/wxcloudrun/backimage.png')
-    wordcloud = WordCloud(mask=bg_pic,background_color='white',font_path='/app/wxcloudrun/华文楷体.ttf',scale=1.5).generate(text)
-
-    wordcloud.to_file('/app/wxcloudrun/filecontent.jpg')
-
     a = duixiangcunchu(courseid)
     b = {"re":a}
     c = json.dumps(b)
